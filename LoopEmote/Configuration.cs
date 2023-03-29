9a12ac8e79756980d5a3f8e0e22959b46f4e125b
Rckii <miska3003@hotmail.com>
2023-03-29 18:29:59 +0300

    loopemote up

---
+++ LoopEmote/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using Dalamud.Configuration;
+using Dalamud.Plugin;
+using System;
+
+namespace LoopEmote
+{
+	[Serializable]
+	public class Configuration : IPluginConfiguration
+	{
+		public int Version { get; set; } = 0;
+
+		public int LoopIntervalInMS { get; set; } = 5000;
+
+		public bool PrintInChat { get; set; } = true;
+
+		// the below exist just to make saving less cumbersome
+		[NonSerialized]
+		private DalamudPluginInterface? PluginInterface;
+
+		public void Initialize(DalamudPluginInterface pluginInterface)
+		{
+			this.PluginInterface = pluginInterface;
+		}
+
+		public void Save()
+		{
+			this.PluginInterface!.SavePluginConfig(this);
+		}
+	}
+}
---
+++ LoopEmote/Windows/ConfigWindow.cs
@@ -0,0 +1,48 @@
+﻿using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace LoopEmote.Windows;
+
+public class ConfigWindow : Window, IDisposable
+{
+	private Configuration Configuration;
+	private Plugin Plugin;
+
+	public ConfigWindow(Plugin plugin) : base(
+	    "Loop Emote Configuration",
+	    ImGuiWindowFlags.NoScrollWithMouse | ImGuiWindowFlags.AlwaysAutoResize)
+	{
+		this.Size = new Vector2(150, 75);
+		this.SizeCondition = ImGuiCond.FirstUseEver;
+
+		this.Configuration = plugin.Configuration;
+		Plugin = plugin;
+	}
+
+	public void Dispose() { }
+
+	public override void Draw()
+	{
+		ImGui.SetNextItemWidth(110.0f);
+
+		var interval = Configuration.LoopIntervalInMS;
+
+		if (ImGui.InputInt("Default Loop Interval In MS", ref interval, 500, 1000))
+		{
+			Configuration.LoopIntervalInMS = Math.Max(interval, Plugin.MIN_INTERVAL);
+			Configuration.Save();
+		}
+
+		var doPrint = Configuration.PrintInChat;
+
+		if(ImGui.Checkbox("Print In Chat", ref doPrint))
+		{
+			Configuration.PrintInChat = doPrint;
+			Configuration.Save();
+
+			Plugin.PrintInChat((doPrint ? "Enabled chat printing like this" : "Disabled chat printing like this"), true);
+		}
+	}
+}
